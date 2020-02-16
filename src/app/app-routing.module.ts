963af516e805ffc36ddd20a0ccdfbab59ff5e82f
Chris Trinh <christrinh5@gmail.com>
2020-02-15 23:59:53 -0800

    Add auth guard without authentication

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,23 +12,28 @@ import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.compon
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeResolverService } from './recipe-resolver.service';
 import { AuthComponent } from './auth/auth.component';
+import { AuthGuard } from './auth/auth.guard';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full'},
-  { path: 'recipes', component: RecipesComponent, children: [
-    { path: '', component: RecipeStartComponent },
-    { path: 'new', component: RecipeEditComponent },
-    { 
-      path: ':id', 
-      component: RecipeDetailsComponent, 
-      resolve: [RecipeResolverService] 
-    },
-    { 
-      path: ':id/edit', 
-      component: RecipeEditComponent, 
-      resolve: [RecipeResolverService] 
-    },
-  ]},
+  { 
+    path: 'recipes',
+    component: RecipesComponent,
+    canActivate: [AuthGuard],
+    children: [
+      { path: '', component: RecipeStartComponent },
+      { path: 'new', component: RecipeEditComponent },
+      { 
+        path: ':id', 
+        component: RecipeDetailsComponent, 
+        resolve: [RecipeResolverService] 
+      },
+      { 
+        path: ':id/edit', 
+        component: RecipeEditComponent, 
+        resolve: [RecipeResolverService] 
+      },
+    ]},
   { path: 'shopping-list', component: ShoppingListComponent},
   { path: 'auth', component: AuthComponent }
 ]
